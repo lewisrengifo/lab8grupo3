a23ed4f6af003c87570b24a9e8e3549dce9ce4ea
Eduardo Sanchez <a20122509@pucp.pe>
2020-06-29 20:01:16 -0500

    eduardo1

--- src/main/java/sw2/lab6/teletok/controller/ControlleerPost.java
+++ src/main/java/sw2/lab6/teletok/controller/ControlleerPost.java
@@ -6,38 +6,56 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
+import sw2.lab6.teletok.entity.PostComment;
+import sw2.lab6.teletok.entity.Token;
 import sw2.lab6.teletok.repository.PostCommentRepository;
+import sw2.lab6.teletok.repository.TokenRepository;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
 public class ControlleerPost {
     @Autowired
     PostCommentRepository postCommentRepository;
-
+    @Autowired
+    TokenRepository tokenRepository;
 
     @PostMapping(value = "/save", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity guardarCharacter(
-            @RequestBody Characters characters ,
-            @RequestParam(value = "fetchId", required = false) boolean fetchId) {
+    public ResponseEntity guardarComentario(@RequestParam("postid")int postId , @RequestParam("token") String token,
+                                            @RequestParam("mesaage")String message,@RequestParam(value = "fetchId", required = false) boolean fetchId) {
         HashMap<String, Object> responseMap = new HashMap<>();
-        charactersRepository.save(characters);
-        if (fetchId) {
-            responseMap.put("id", characters.getId());
+        PostComment postComment = new PostComment();
+        Token auxtoken= tokenRepository.darToken(token);
+        if(auxtoken!=null){
+            if(postId>0 && message!=null){
+                postComment.getPost().setId(postId);
+                postComment.setMessage(message);
+                postComment.getUser().setId(auxtoken.getUser().getId());
+                postCommentRepository.save(postComment);
+                responseMap.put("id",postComment.getId());
+                return new ResponseEntity(responseMap, HttpStatus.CREATED);
+            }
         }
-        responseMap.put("estado", "creado");
+        else{
+            responseMap.put("error", "TOKEN_INVALID");
+            return new ResponseEntity(responseMap, HttpStatus.CREATED);
+        }
+        responseMap.put("error", "POST_NOT_FOUND");
         return new ResponseEntity(responseMap, HttpStatus.CREATED);
+
     }
-    @ExceptionHandler(HttpMessageNotReadableException.class)
+
+    /*@ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity gestionExcepcion(HttpServletRequest request) {
 
         HashMap<String, Object> responseMap = new HashMap<>();
         if (request.getMethod().equals("POST")) {
             responseMap.put("estado", "error");
-            responseMap.put("msg", "Debe enviar un Character");
+            responseMap.put("msg", "Debe enviar un Comentario Completo");
         }
         return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
-    }
+    }*/
 }
--- src/main/java/sw2/lab6/teletok/repository/TokenRepository.java
+++ src/main/java/sw2/lab6/teletok/repository/TokenRepository.java
@@ -1,9 +1,14 @@
 package sw2.lab6.teletok.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import sw2.lab6.teletok.entity.Token;
 
+import java.util.List;
+
 @Repository
 public interface TokenRepository extends JpaRepository<Token, Integer> {
+    @Query(value = "select * from teletok.token where token.code=?1", nativeQuery = true)
+    Token darToken(String cod);
 }
